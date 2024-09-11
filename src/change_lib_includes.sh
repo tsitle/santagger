e1eeefbd591b91f88a3b97e9cfc836b230193588
Thomas <technisandk@gmail.com>
2024-09-11 20:30:03 +0200

    Move change_lib_includes.sh to src/

--- src/change_lib_includes.sh
+++ src/change_lib_includes.sh
@@ -6,6 +6,7 @@
 
 find . -type f -regex '.*\.[hc]' -print0 |
 		while IFS= read -r -d '' TMP_FN; do
+			echo "-- $TMP_FN"
 			sed 's|#include <santag/\(.*\)\.h>|#include "src/includes/\1.h"|' "$TMP_FN" > "${TMP_FN}-$$" || exit 1
 			mv "${TMP_FN}-$$" "${TMP_FN}"
 		done
