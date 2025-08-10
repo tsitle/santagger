d1df00ec2daee45a0e953ca14a98b75f7000060f
Thomas <technisandk@gmail.com>
2025-08-10 12:49:21 +0200

    change_header_comments.sh: Process *.h.in files

--- src/change_header_comments.sh
+++ src/change_header_comments.sh
@@ -4,9 +4,13 @@
 #
 # by TS, Sep 2024
 
-find . -type f -regex '.*\.[hc]' -print0 |
-		while IFS= read -r -d '' TMP_FN; do
-			echo "-- $TMP_FN"
-			sed 's|(C) 2005-2024 by Thomas Sandkuehler (technisandk@gmail.com)|(C) 2005-2025 by Thomas Sandkuehler (technisandk@gmail.com)|' "$TMP_FN" > "${TMP_FN}-$$" || exit 1
-			mv "${TMP_FN}-$$" "${TMP_FN}"
-		done
+function replaceHeaderStuff() {
+	while IFS= read -r -d '' TMP_FN; do
+		echo "-- $TMP_FN"
+		sed 's|(C) 2005-2024 by Thomas Sandkuehler (technisandk@gmail.com)|(C) 2005-2025 by Thomas Sandkuehler (technisandk@gmail.com)|' "$TMP_FN" > "${TMP_FN}-$$" || exit 1
+		mv "${TMP_FN}-$$" "${TMP_FN}"
+	done
+}
+
+find . -type f -regex '.*\.[hc]' -print0 | replaceHeaderStuff
+find . -type f -regex '.*\.h\.in' -print0 | replaceHeaderStuff
